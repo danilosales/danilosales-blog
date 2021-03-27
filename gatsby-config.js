0c24df901d91c0f75c71691bc68fa1607ae43cf6
Danilo Sales <danilosales.info@gmail.com>
2021-03-27 15:53:42 -0300

    Cleanup starter stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Danilo Sales blog`,
+    description: `Um blog sobre diversas tecnologias de desenvolvimento`,
+    author: `@danilosalesinfo`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
---
+++ src/components/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+type LayoutProps  = {
+  children: React.ReactNode
+}
+
+const Layout = ({ children }: LayoutProps) => {
+
+  return (
+    <main>{children}</main>
+  )
+}
+
+export default Layout
