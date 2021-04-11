1facd0f29a93b675feb24bf1c9e94bcc37a84192
Danilo Sales <danilosales.info@gmail.com>
2021-04-11 11:58:19 -0300

    feat: create a servless function on vercel to integrate netlifycms with github to authenticate

---
+++ api/auth.ts
@@ -0,0 +1,24 @@
+import { IncomingMessage, ServerResponse } from 'http'
+import { AuthorizationCode } from 'simple-oauth2'
+import { randomBytes } from 'crypto'
+import { config } from '../lib/config'
+
+export const randomString = () => randomBytes(4).toString('hex')
+
+export default async (req: IncomingMessage, res: ServerResponse) => {
+  const { host } = req.headers
+  const url = new URL(`https://${host}/${req.url}`)
+  const urlParams = url.searchParams
+  const provider = urlParams.get('provider')
+
+  const client = new AuthorizationCode(config)
+
+  const authorizationUri = client.authorizeURL({
+    redirect_uri: `https://${host}/callback?provider=${provider}`,
+    scope: 'repo,user',
+    state: randomString()
+  })
+
+  res.writeHead(301, { Location: authorizationUri })
+  res.end()
+}
